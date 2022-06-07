8f7ea6039df1411e75019dccfe4c7a6d1cb91510
beckydvn <18rldv@queensu.ca>
2022-06-07 18:09:46 -0400

    finish base parsing

--- json_config_parser.py
+++ json_config_parser.py
@@ -1,10 +1,9 @@
+from lib2to3.pytree import convert
 import yaml
 import json
 
-
 def parse_to_json_config(filename: str):
     loaded_yaml = yaml.load(open(filename, "r"), Loader=yaml.FullLoader)
-
     json_config = {}
     json_config["name"] = loaded_yaml["name"]
     # convert context variables
@@ -37,7 +36,7 @@ def parse_to_json_config(filename: str):
         json_config["intents"][intent_name]["utterances"] = intent["examples"]
     # convert actions
     json_config["actions"] = {act: {} for act in loaded_yaml["actions"]}
-    for act in json_config["actions"]:
+    for act in loaded_yaml["actions"]:
         yaml_act = loaded_yaml["actions"][act]
         # load in name, type, subtype, and message variants
         json_config["actions"][act]["name"] = act
@@ -93,15 +92,15 @@ def parse_to_json_config(filename: str):
                             "variable": yaml_act["effects"][effect]["entity"],
                             "value": f"${yaml_act['effects'][effect]['entity']}",
                             "certainty": "Uncertain",
-                            "follow_up": f"clarify__{act}",
                         }
                     },
                 }
-                unclear["unclear-intent"] = (
+                unclear["intent"] = (
                     yaml_act["effects"][effect]["unclear-intent"]
                     if "unclear-intent" in effect
                     else None
                 )
+                unclear["follow_up"] = f"clarify__{act}"
                 converted_eff["outcomes"] = [valid, unclear]
             elif effect == "yes-no":
                 converted_eff["global-outcome-name"] = "yes-no"
@@ -128,8 +127,81 @@ def parse_to_json_config(filename: str):
                     },
                     "intent": "deny",
                 }
-                converted_eff["outcomes"] = [confirm, deny]                
+                converted_eff["outcomes"] = [confirm, deny]
+            else:
+                for option in yaml_act["effects"][effect]:
+                    converted_eff["global-outcome-name"] = effect
+                    converted_eff["type"] = option
+                    outcomes = yaml_act["effects"][effect][option]["outcomes"]
+                    outcomes_list = []
+                    for out, out_config in outcomes.items():
+                        next_outcome = {}
+                        next_outcome["name"] = out
+                        updates = out_config["updates"] if "updates" in out_config else None
+                        if updates:
+                            collect_updates = {}
+                            for update, update_config in updates.items():
+                                collect_updates[update] = {
+                                        "variable": update,
+                                        # if the value isn't supplied then the user is likely just setting "known" to False
+                                        "value": update_config["value"] if "value" in update_config else None
+                                    }
+                                if update_config["known"] if "known" in update_config else None != None:
+                                    if status:
+                                        status = "Known"
+                                    elif not status:
+                                        status = "Unknown"
+                                    elif status == "maybe":
+                                        status = "Uncertain"
+                                    collect_updates[update]["certainty"] = status
+                            next_outcome["updates"] = collect_updates
+                        next_outcome["intent"] = out_config["intent"] if "intent" in out_config else None
+                        next_outcome["follow_up"] = out_config["follow_up"] if "follow_up" in out_config else None
+                        if "status" in out_config:
+                            next_outcome["status"] = out_config["status"]
+                        if "response" in out_config:
+                            next_outcome["response"] = out_config["response"]
+                        if "call" in out_config:
+                            next_outcome["call"] = out_config["call"]
+                        outcomes_list.append(next_outcome)
+                    converted_eff["outcomes"] = outcomes_list
             json_config["actions"][act]["effect"] = converted_eff
+        if "clarify" in yaml_act:
+            clarify_act = {}
+            entity = yaml_act["clarify"]["entity"]
+            clarify_act["name"] = f"clarify__{act}"
+            clarify_act["type"] = yaml_act["type"]
+            clarify_act["subtype"] = yaml_act["subtype"]
+            clarify_act["message_variants"] = yaml_act["clarify"]["message_variants"]
+            clarify_act["condition"] = [[entity, "Uncertain"]]
+            clarify_act["effect"] = {
+                    "type": "oneof",
+                    "global-outcome-name": "yes-no",
+                        "outcomes": [
+                            {
+                                "name": "confirm",
+                                "updates": {
+                                    entity: {
+                                        "variable": entity,
+                                        "value": f"${entity}",
+                                        "certainty": True
+                                    }
+                                },
+                                "intent": "confirm"
+                            },
+                            {
+                                "name": "deny",
+                                "updates": {
+                                    entity: {
+                                        "value": None,
+                                        "certainty": False
+                                    }
+                                },
+                                "intent": "deny"
+                            }
+                        ]
+                    }
+        json_config["actions"][f"clarify__{act}"] = clarify_act
     return json_config
 
 
