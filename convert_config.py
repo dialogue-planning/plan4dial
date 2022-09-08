f11f59a8bd2aa07367b8c0cf2f3c526f4a17a636
beckydvn <18rldv@queensu.ca>
2022-09-08 18:45:14 -0400

    remove domain name from file generation

--- convert_config.py
+++ convert_config.py
@@ -12,40 +12,39 @@ from plan4dial.parsing.pddl_for_rollout import rollout_config
 
 
 def generate_files(
-    yaml_filename: str, domain_name: str, rbp_path: str, train: bool = False
+    yaml_filename: str, output_folder: str, rbp_path: str, train: bool = False
 ):
-    dirname = f"./output_files/{domain_name}"
     # make a new directory for this domain if it doesn't exist
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
     # convert to hovor json config
-    writer = open(f"{dirname}/{domain_name}.json", "w")
+    writer = open(f"{output_folder}/configuration_data.json", "w")
     converted_json = convert_yaml(yaml_filename)
     writer.write(json.dumps(converted_json, indent=4))
     # convert to PDDL
     domain, problem = parse_to_pddl(converted_json)
-    domain_str, problem_str = f"{dirname}/{domain_name}_domain.pddl", f"{dirname}/{domain_name}_problem.pddl"
+    domain_str, problem_str = f"{output_folder}/domain.pddl", f"{output_folder}/problem.pddl"
     writer = open(domain_str, "w")
     writer.write(domain)
     writer = open(problem_str, "w")
     writer.write(problem)
-    writer = open(f"{dirname}/{domain_name}_nlu.yml", "w")
+    writer = open(f"{output_folder}/nlu.yml", "w")
 
-    # # train rasa NLU model
-    # if train:
-    #     # parse for rasa
-    #     yaml.dump(make_nlu_file(yaml_filename), writer)
-    #     # download the spacy model if needed; wait until complete
-    #     try:
-    #         spacy.load("en_core_web_md")
-    #     except OSError:
-    #         subprocess.run(["python -m spacy download en_core_web_md"])
-    #     train_nlu(
-    #         config=f"./output_files/config.yml",
-    #         nlu_data=f"{dirname}/{domain_name}_nlu.yml",
-    #         output=f"{dirname}",
-    #         fixed_model_name=f"{domain_name}-model"
-    #     )
+    # train rasa NLU model
+    if train:
+        # parse for rasa
+        yaml.dump(make_nlu_file(yaml_filename), writer)
+        # download the spacy model if needed; wait until complete
+        try:
+            spacy.load("en_core_web_md")
+        except OSError:
+            subprocess.run(["python -m spacy download en_core_web_md"])
+        train_nlu(
+            config=f"./output_files/config.yml",
+            nlu_data=f"{output_folder}/nlu.yml",
+            output=f"{output_folder}",
+            fixed_model_name=f"nlu_model"
+        )
     # generate PDDL files; convert policy.out to a prp.json file; wait until complete
     subprocess.run([f"{rbp_path}/prp", domain_str, problem_str, "--output-format", "3"])
     try:
@@ -53,21 +52,21 @@ def generate_files(
             plan_data = {f"plan": json.load(f)}
     except FileNotFoundError:
         raise Exception("PDDL is invalid.")
-    with open(f"{dirname}/{domain_name}.prp.json", "w") as f:
+    with open(f"{output_folder}/plan_data.prp.json", "w") as f:
         json.dump(plan_data, f, indent=4)
     # delete extra output files
     os.remove("./policy.out")
     os.remove("./output.sas")
     # for rollout
     rollout_data = rollout_config(converted_json)
-    with open(f"{dirname}/{domain_name}_rollout_config.json", "w") as f:
+    with open(f"{output_folder}/rollout_config.json", "w") as f:
         json.dump(rollout_data, f, indent=4)
 
 
 if __name__ == "__main__":
     generate_files(
         "./plan4dial/yaml_samples/tutorial_bot_simplified.yml",
-        "tutorial_bot_simplified",
+        "./output_files/tutorial_bot_simplified",
         str((Path(__file__).parent.parent / "rbp").resolve()),
         False
     )
