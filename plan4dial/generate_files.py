8969f7a0f801ea92745a38ec8774754ca1ec1ced
beckydvn <18rldv@queensu.ca>
2022-09-08 22:18:45 -0400

    clean up test run

--- plan4dial/generate_files.py
+++ plan4dial/generate_files.py
@@ -2,13 +2,13 @@ import json
 import yaml
 import os
 import subprocess
-#import spacy
+import spacy
 from pathlib import Path
 from plan4dial.parsers.json_config_parser import convert_yaml
 from plan4dial.parsers.pddl_parser import parse_to_pddl
 from plan4dial.parsers.parse_for_rasa import make_nlu_file
 from plan4dial.parsers.pddl_for_rollout import rollout_config
-#from rasa.model_training import train_nlu
+from rasa.model_training import train_nlu
 
 
 def generate_files(
@@ -28,23 +28,23 @@ def generate_files(
     writer.write(domain)
     writer = open(problem_str, "w")
     writer.write(problem)
-    writer = open(f"{output_folder}/nlu.yml", "w")
-
+    
     # train rasa NLU model
-    # if train:
-    #     # parse for rasa
-    #     yaml.dump(make_nlu_file(yaml_filename), writer)
-    #     # download the spacy model if needed; wait until complete
-    #     try:
-    #         spacy.load("en_core_web_md")
-    #     except OSError:
-    #         subprocess.run(["python -m spacy download en_core_web_md"])
-    #     train_nlu(
-    #         config=f"nlu_config.yml",
-    #         nlu_data=f"{output_folder}/nlu.yml",
-    #         output=f"{output_folder}",
-    #         fixed_model_name=f"nlu_model"
-    #     )
+    if train:
+        writer = open(f"{output_folder}/nlu.yml", "w")
+        # parse for rasa
+        yaml.dump(make_nlu_file(yaml_filename), writer)
+        # download the spacy model if needed; wait until complete
+        try:
+            spacy.load("en_core_web_md")
+        except OSError:
+            subprocess.run(["python -m spacy download en_core_web_md"])
+        train_nlu(
+            config=f"nlu_config.yml",
+            nlu_data=f"{output_folder}/nlu.yml",
+            output=f"{output_folder}",
+            fixed_model_name=f"nlu_model"
+        )
     # generate PDDL files; convert policy.out to a prp.json file; wait until complete
     subprocess.run([f"{rbp_path}/prp", domain_str, problem_str, "--output-format", "3"])
     try:
@@ -64,9 +64,10 @@ def generate_files(
 
 
 if __name__ == "__main__":
+    dirname = "./plan4dial/local_data/gold_standard_bot"
     generate_files(
-        "./plan4dial/local_data/gold_standard_bot/gold_standard_bot.yml",
-        "./plan4dial/local_data/gold_standard_bot/output_files",
+        f"{dirname}/gold_standard_bot.yml",
+        f"{dirname}/output_files",
         str((Path(__file__).parent.parent / "rbp").resolve()),
         True
     )
