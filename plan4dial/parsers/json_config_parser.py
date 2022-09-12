ed7558d236e138d1eb25ba2d9652b93afbf1db6a
beckydvn <18rldv@queensu.ca>
2022-09-12 15:08:56 -0400

    also try italic

--- plan4dial/parsers/json_config_parser.py
+++ plan4dial/parsers/json_config_parser.py
@@ -13,7 +13,7 @@ def test():
 
     # Args:  
         **table_handle**: An open smalltable.Table instance.  
-        keys: A sequence of strings representing the key of each table
+        *keys*: A sequence of strings representing the key of each table
           row to fetch.  String keys will be UTF-8 encoded.  
         require_all_keys: If True only rows with values set for all keys will be
           returned.  
