90bb7304b4691288917cb41ce7b6807ec7e098df
beckydvn <18rldv@queensu.ca>
2022-10-03 22:51:22 -0400

    add plan4dial path explicitly

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../plan4dial/plan4dial'))  # Source code dir relative to this file
-sys.path.insert(0, os.path.abspath('../..'))  # outer directory
+sys.path.insert(0, os.path.abspath('../plan4dial'))  # outer directory
+#sys.path.insert(0, os.path.abspath('../..'))
 
 # Turn on sphinx.ext.autosummary and set autodoc options
 autosummary_generate = True
