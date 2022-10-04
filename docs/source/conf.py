ad16e901c1f008d1fc41a233be0e82aa6d00afbc
beckydvn <18rldv@queensu.ca>
2022-10-03 23:15:42 -0400

    try adding trailing slash to conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,9 +8,9 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../plan4dial/plan4dial'))  # Source code dir relative to this file
-sys.path.insert(0, os.path.abspath('../plan4dial'))  # outer directory
-sys.path.insert(0, os.path.abspath('../..')) # home path
+sys.path.append(os.path.abspath('../plan4dial/plan4dial/'))  # Source code dir relative to this file
+sys.path.append(os.path.abspath('../plan4dial/'))  # outer directory
+sys.path.append(os.path.abspath('../../')) # home path
 
 # Turn on sphinx.ext.autosummary and set autodoc options
 autosummary_generate = True
