506cebafa96a0b923b1b6a041e0bd6b9ad4b5cfc
beckydvn <18rldv@queensu.ca>
2022-10-06 12:08:57 -0400

    delete css width formatting addition

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,48 +8,47 @@
 
 import sys
 import pathlib
+
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
-sys.path.insert(0, (pathlib.Path(__file__).parents[2] / "plan4dial").resolve().as_posix())
+sys.path.insert(
+    0, (pathlib.Path(__file__).parents[2] / "plan4dial").resolve().as_posix()
+)
 print(sys.path)
 
 
 # Turn on sphinx.ext.autosummary and set autodoc options
 autosummary_generate = True
 autodoc_default_options = {
-    'members':          True,
-    'undoc-members':    True,
-    'private-members':  True,
-    'special-members':  True,
-    'inherited-members':True,
-    'show-inheritance': True
+    "members": True,
+    "undoc-members": True,
+    "private-members": True,
+    "special-members": True,
+    "inherited-members": True,
+    "show-inheritance": True,
 }
 
-project = 'Plan4Dial'
-copyright = '2022, Rebecca De Venezia @ QuMuLab'
-author = 'Rebecca De Venezia @ QuMuLab'
-release = '1.0.0'
+project = "Plan4Dial"
+copyright = "2022, Rebecca De Venezia @ QuMuLab"
+author = "Rebecca De Venezia @ QuMuLab"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-   'sphinx.ext.autodoc', # gets docstrings from Python code
-   'sphinx.ext.autosummary', # auto-generates .rst files
-   'sphinx.ext.napoleon', # allows for Google/numpy style docstrings
-   'sphinx.ext.viewcode', # adds link to the source code in the docs
-   'sphinx_rtd_theme'
+    "sphinx.ext.autodoc",  # gets docstrings from Python code
+    "sphinx.ext.autosummary",  # auto-generates .rst files
+    "sphinx.ext.napoleon",  # allows for Google/numpy style docstrings
+    "sphinx.ext.viewcode",  # adds link to the source code in the docs
+    "sphinx_rtd_theme",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-html_css_files = [
-    'custom.css',
-]
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
